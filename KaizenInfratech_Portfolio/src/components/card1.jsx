ddf734b8262d632cceaa3276908cfc1623337c39
Harshil Arora <Harshilarora0@gmail.com>
2026-01-20 23:25:23 -0800

    home update

--- KaizenInfratech_Portfolio/src/components/card1.jsx
+++ KaizenInfratech_Portfolio/src/components/card1.jsx
@@ -1,16 +1,54 @@
-import React from 'react'
+import React from "react";
 
-function card1({Title, description}) {
-    
-
-
+function Card1({ Title, description, url }) {
   return (
-    <div className="bg-[#f0f0f0] p-4">
-        <h3 className="text-lg font-semibold mb-2 font-primary">{Title}</h3>
-        <p className="text-sm text-gray-600 font-secondary">{description}</p>
+    <div
+      className="
+        relative
+        w-[320px] h-[240px]
+        bg-[#f0f0f0]
+        px-8 pt-8 pb-14
+        rounded-lg
+       
+      "
+    >
+      {/* Background Icon */}
+      <img
+        src={url}
+        alt="card icon"
+        className="
+          absolute
+          right-0 top-8
+          w-52 h-52
+          opacity-5
+          pointer-events-none
+          z-1
+        "
+      />
+
+      {/* Content */}
+      <h3 className="text-md font-md font-primary mt-23 relative z-10">
+        {Title}
+      </h3>
+
+      <p className="text-[0.725rem] text-gray-600 font-secondary font-light pt-2 relative z-10">
+        {description}
+      </p>
+
+      {/* Arrow */}
+      {/* <div
+        className="
+          absolute bottom-6 right-6
+          w-9 h-9
+          border-2 border-yellow-400
+          flex items-center justify-center
+          text-lg
+        "
+      >
+        →
+      </div> */}
     </div>
-    
-  )
+  );
 }
 
-export default card1
+export default Card1;
--- KaizenInfratech_Portfolio/src/pages/Home/Home.jsx
+++ KaizenInfratech_Portfolio/src/pages/Home/Home.jsx
@@ -44,7 +44,7 @@ import Card from "../../components/card1.jsx";
                 </div>
                 <div>
                 <h4 className="font-primary font-bold ">Our Mission</h4>
-                <p className="w-4/5 font-secondary font-extralight text-sm mt-2">Our mission is to shape communities by delivering durable, efficient, and future-ready construction solutions.</p>
+                <p className="w-4/5 font-secondary font-extralight text-xs mt-2">Our mission is to shape communities by delivering durable, efficient, and future-ready construction solutions.</p>
                 </div>
               </div>
 
@@ -54,7 +54,7 @@ import Card from "../../components/card1.jsx";
                 </div>
                 <div>
                 <h4 className="font-primary font-bold ">Our Vision</h4>
-                <p className="w-4/5 font-secondary font-extralight text-sm mt-2">Our vision is to be a trusted leader in civil engineering, shaping infrastructure that drives progress across regions.</p>
+                <p className="w-4/5 font-secondary font-extralight text-xs mt-2">Our vision is to be a trusted leader in civil engineering, shaping infrastructure that drives progress across regions.</p>
                 </div>
               </div>
 
@@ -72,12 +72,33 @@ import Card from "../../components/card1.jsx";
           </div>
         </section>
 
-        <section className="w-4/5 mx-auto bg-[#fcfcfc] h-screen grid grid-cols-3 grid-rows-2 justify-center border justify-items-center border-gray-300 px-5 ">
-          <div>
+        <section className="w-4/5 mx-auto bg-[#fcfcfc] h-screen grid grid-cols-3 grid-rows-2 justify-center items-center border justify-items-center border-gray-300 px-5 ">
+          <div className=" ">
             <h2 className=" text-xl sm:text-4xl py-10 leading-10 font-primary font-md text-font text-left mt-15">What We Deliver.</h2>
             <p className="text-font font-secondary font-light text-sm border-l-2 border-primary px-2 max-w-4/5">We deliver civil and infrastructure construction solutions engineered for complex systems, including gas stations and depots, utility facilities, industrial factory setups, and residential and commercial infrastructure.</p>
 
           </div>
+
+          <div>
+            <Card 
+            Title="General Construction" 
+            description="services encompassing commercial and residential renovations, structural upgrades, interior fit-outs, and complete project execution." 
+            url="../src/assets/images/cardicon1.png" 
+            />
+          </div>
+
+          <div>
+            <Card Title="Card Title" description="This is a description for the card component. It provides additional information about the content of the card." />
+          </div>
+
+          <div>
+            <Card Title="Card Title" description="This is a description for the card component. It provides additional information about the content of the card." />
+          </div>
+
+          <div>
+            <Card Title="Card Title" description="This is a description for the card component. It provides additional information about the content of the card." />
+          </div>
+
           <div>
             <Card Title="Card Title" description="This is a description for the card component. It provides additional information about the content of the card." />
           </div>
